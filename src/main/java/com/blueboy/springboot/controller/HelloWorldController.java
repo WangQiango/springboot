50e9ae5103be3bff61d427d9c1077a9e43a69521
wangqiang <1091282108@qq.com>
2020-09-07 20:39:48 +0800

    提交：用反射修改String对象的值

--- src/main/java/com/blueboy/springboot/controller/HelloWorldController.java
+++ src/main/java/com/blueboy/springboot/controller/HelloWorldController.java
@@ -11,7 +11,7 @@ public class HelloWorldController {
 
     //如果我们只需要在某个业务逻辑中获取一下配置文件中的某个值，就用@Value
     //如果我们专门编写了一个javaBean来和配置文件进行映射的话，那就直接使用@ConfigurationProperties；
-    @Value("${person.name}")
+//    @Value("${person.name}")
     private String name;
     @RequestMapping("/hello")
     public String helloWorld(){
