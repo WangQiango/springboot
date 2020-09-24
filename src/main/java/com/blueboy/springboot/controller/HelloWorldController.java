5b059d2711df49fc4d53cadb683e941e9c7afd6a
wangqiang <1091282108@qq.com>
2020-09-24 15:35:41 +0800

    提交代码：git拉取远程仓库代码到本地文件夹

--- src/main/java/com/blueboy/springboot/controller/HelloWorldController.java
+++ src/main/java/com/blueboy/springboot/controller/HelloWorldController.java
@@ -1,8 +1,10 @@
 package com.blueboy.springboot.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +23,13 @@ public class HelloWorldController {
     @RequestMapping("sayHello")
     public String sayHello(){
         return "hello " + name;
+    }
+
+    @RequestMapping("say")
+    @ResponseBody
+    public String say(@RequestBody String body){
+        System.out.println(body);
+        JSONObject jb = JSON.parseObject(body);
+        return "hello " + 12;
     }
 }
--- src/main/java/com/blueboy/springboot/service/HelloService.java
+++ src/main/java/com/blueboy/springboot/service/HelloService.java
@@ -1,4 +1,13 @@
 package com.blueboy.springboot.service;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.net.http.HttpClient;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloService {
+    public static void main(String[] args) {
+
+    }
 }
