f1faf970adf392213d0a2a053c38a312a26d5ca1
wangqiang <1091282108@qq.com>
2020-09-30 15:50:50 +0800

    thyeleaf的基本语法使用

--- springboot-web-restfulcrud/src/main/java/com/wangqiang/springboot/controller/HelloController.java
+++ springboot-web-restfulcrud/src/main/java/com/wangqiang/springboot/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Map;
 
 @Controller
@@ -26,7 +27,8 @@ public class HelloController {
     //查出一些数据在页面展示
     @RequestMapping("/success")
     public String success(Map map){
-        map.put("hello","你好");
+        map.put("hello","<h2>你好</h2>");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu","zhaoliu"));
         return "success";
     }
 
